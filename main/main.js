7dd7e7c8f001317ebf05c4d540f326f8df9f9f23
Yannis <standinthesun@sina.com>
2018-07-30 22:02:27 +0800

    Finish Task2

--- main/main.js
+++ main/main.js
@@ -1,18 +1,46 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.getall()}
+o) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
-  constructor(input) {
-    // Write your code here
+  constructor(Obj) {
+    this.Obj = Obj;
   }
-
+  
   minimum() {
-    // Write your code here
+    var min = this.Obj[0];
+    for (var i = 1; i < this.Obj.length; i++){
+      if (this.Obj[i] < min ){
+        min = this.Obj[i];
+      }
+    }
+    return min;
   }
 
-  // Write your code here
+  maximum() {
+    var max = this.Obj[0];
+    for (var i = 1; i < this.Obj.length; i++){
+      if (this.Obj[i] > max ){
+        max = this.Obj[i];
+      }
+    }
+    return max;
+  }
+
+  getall(){
+    return this.Obj.length;
+  }
+
+  average(){
+    var sum = this.Obj[0];
+    for (var i = 1; i < this.Obj.length; i++){
+      sum += this.Obj[i];
+    }
+    var average = sum / (this.Obj.length);
+    return average.toFixed(2);
+  }
 }
